22ea8ff9bf2a935645a1b3667d948898e00080ea
Ryan Mathews <ryanmathews@meta.com>
2024-12-22 20:28:21 -0600

    Move parsing logic to networker

--- api/solscan_networker.py
+++ api/solscan_networker.py
@@ -1,4 +1,8 @@
+import json
+
 from api.base_networker import BaseNetworker
+from models.owner import Owner
+from models.token import Token
 from utils.constants import SOLSCAN_BASE_URL
 
 HEADERS = {"token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE3MzQ5MTU1NTcxNjUsImVtYWlsIjoicm1hdGh0cmFkaW5nQGdtYWlsLmNvbSIsImFjdGlvbiI6InRva2VuLWFwaSIsImFwaVZlcnNpb24iOiJ2MiIsImlhdCI6MTczNDkxNTU1N30.UmkCER3DAtMR2HPn535Dw9CHz6N1e1gAKjZG2wxh3GM"}
@@ -12,37 +16,59 @@ class SolscanNetworker:
                                   address: str,
                                   page: int = 1,
                                   page_size: int = 10):
+
         params = {
             "address": address,
             "type": "token",
             "page": page,
             "page_size": page_size
         }
+
         try:
-            return self.base_networker.get(
+
+            response = self.base_networker.get(
                 "account/token-accounts", params=params, headers=HEADERS)
+            tokens_response = json.loads(json.dumps(response["data"]))
+            tokens = [Token.from_dict(token) for token in tokens_response]
+
+            token_metadata = response["metadata"]["tokens"]
+            for token in tokens:
+                token_metadata_info = token_metadata.get(token.address, {})
+                token.set_name(token_metadata_info.get("name", ""))
+                token.set_symbol(token_metadata_info.get("symbol", ""))
+
+            return tokens
+
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
 
-    def getTopHoldersForToken(self,
-                              token: str,
-                              page: int = 1,
-                              page_size: int = 10,
-                              from_amount: str = "",
-                              to_amount: str = ""):
+    def getTopOwnersOfToken(self,
+                            token: str,
+                            page: int = 1,
+                            page_size: int = 10,
+                            from_amount: str = "",
+                            to_amount: str = ""):
         params = {
             "address": token,
             "page": page,
             "page_size": page_size
         }
+
         if from_amount:
             params["from_amount"] = from_amount
         if to_amount:
             params["to_amount"] = to_amount
+
         try:
-            return self.base_networker.get(
+
+            response = self.base_networker.get(
                 "token/holders", params=params, headers=HEADERS)
+            response_data = json.loads(json.dumps(response["data"]["items"]))
+            owners = [Owner.from_dict(owner) for owner in response_data]
+
+            return owners
+
         except Exception as e:
             print(f"An error occurred: {e}")
             return None
--- main.py
+++ main.py
@@ -3,36 +3,19 @@ This module demonstrates the usage of the
 TokenNetworker class to fetch top traders from the Bitquery API.
 """
 
-import json
-
-from utils.printer import PrintingUtils
-from models.owner import Owner
-from models.token import Token
 from api.solscan_networker import SolscanNetworker
+from utils.printer import PrintingUtils
 
 if __name__ == "__main__":
 
     networker = SolscanNetworker()
-    json_data = networker.getTokensForWalletAddress(
-        "4yCqEknZEJwPngkAaX4F2peVUGGhWB82kwBKK7LHGg1A")
-    data_str = json.dumps(json_data["data"])
-    data_dict = json.loads(data_str)
 
-    tokens = [Token.from_dict(token) for token in data_dict]
-    token_metadata = json_data["metadata"]["tokens"]
-    for token in tokens:
-        token_metadata_info = token_metadata.get(token.address, {})
-        token.set_name(token_metadata_info.get("name", ""))
-        token.set_symbol(token_metadata_info.get("symbol", ""))
+    tokens = networker.getTokensForWalletAddress(
+        "4yCqEknZEJwPngkAaX4F2peVUGGhWB82kwBKK7LHGg1A")
 
     PrintingUtils.print_tokens_table(tokens=tokens)
 
-    holder_json_data = networker.getTopHoldersForToken(
+    owners = networker.getTopOwnersOfToken(
         "B8q1emW1dKLerpaiLi9n4ZVSCMLaDZUe8T6mPekPpump")
-    holders_data_items = json.dumps(holder_json_data["data"]["items"])
-    holders_data = json.loads(holders_data_items)
 
-    holders = [Owner.from_dict(holder)
-               for holder in holders_data]
-
-    PrintingUtils.print_owners_table(owners=holders)
+    PrintingUtils.print_owners_table(owners=owners)
